6b8372f29747340b0f3d598e23dabdad2659ffed
nathenharvey <nharvey@chef.io>
2014-12-20 22:26:01 -0500

    spell 'temperature' correctly

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,12 +3,11 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
-
 search('cookies_ingredients', '*:*').each do |cookie_type|
   cookies_cookie cookie_type['id'] do
     ingredients cookie_type['ingredients']
     bake_time 10
-    temprature 350
+    temperature 350
     action :create
   end
 end
