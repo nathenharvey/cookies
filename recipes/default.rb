e89f8ab887f6c1bdead8f75fdbd12a97f758529d
nathenharvey <nharvey@chef.io>
2014-12-20 22:35:11 -0500

    eat all the cookies!

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,5 @@
-#
-# Cookbook Name:: cookies
-# Recipe:: default
-#
-# Copyright (c) 2014 The Authors, All Rights Reserved.
-search('cookies_ingredients', '*:*').each do |cookie_type|
+search("cookies_ingredients", "*:*").each do |cookie_type|
   cookies_cookie cookie_type['id'] do
-    ingredients cookie_type['ingredients']
-    bake_time 10
-    temperature 350
-    action :create
+    action :eat
   end
 end
