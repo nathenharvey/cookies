4e0b0577a7317925190b035c736378f070ed7abb
nathenharvey <nharvey@chef.io>
2014-12-20 22:23:23 -0500

    Add an eat action and write a file with the create action

--- providers/cookie.rb
+++ providers/cookie.rb
@@ -1,5 +1,23 @@
 use_inline_resources
 
 action :create do
-  log "My name is #{new_resource.name}"
+ 
+  template "/tmp/#{new_resource.name}" do
+    source "basic_recipe.erb"
+    mode "0644"
+    variables(
+      :ingredients => new_resource.ingredients,
+      :bake_time   => new_resource.bake_time,
+      :temperature => new_resource.temperature,
+      :name        => new_resource.name,
+     )
+  end
+end
+ 
+action :eat do
+ 
+  file "/tmp/#{new_resource.name}" do
+    action :delete
+  end
+ 
 end
